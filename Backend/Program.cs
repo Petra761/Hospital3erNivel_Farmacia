471c0d2eac52a0cf6b913abe65c43ba8c64cf964
Herberth <herberthmamani761@gmail.com>
2026-05-05 18:52:20 -0400

    Agregado la mirgracion automatica

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -24,6 +24,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
