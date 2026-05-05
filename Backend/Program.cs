4c4f912ceebfd940695afab5eefa8160d2351d9d
Herberth <herberthmamani761@gmail.com>
2026-05-05 19:24:44 -0400

    correccion error

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("ConnectionStrings__Connection"))
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
 builder.Services.AddCors(options =>
