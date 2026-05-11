d1e0582475bf58ce3e74898bc6119eb56cb85e9e
Herberth <herberthmamani761@gmail.com>
2026-05-11 19:32:31 -0400

    migrado a .net 10 y agregado el scalar

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,9 +1,20 @@
 using Data;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+if (!builder.Environment.IsDevelopment())
+{
+    var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+    builder.WebHost.UseUrls($"http://*:{port}");
+}
+
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
@@ -12,15 +23,9 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(
         "AllowAll",
-        policy =>
-        {
-            policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
-        }
+        policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
     );
 });
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -29,22 +34,22 @@ using (var scope = app.Services.CreateScope())
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate();
 }
+
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/openapi/v1.json", "API v1");
+});
+app.MapScalarApiReference();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseHttpsRedirection();
 }
-
-if (!builder.Environment.IsDevelopment())
-{
-    var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-    builder.WebHost.UseUrls($"http://*:{port}");
-}
-
-app.UseSwagger();
-app.UseSwaggerUI();
+else { }
 
 app.UseCors("AllowAll");
-
 app.UseAuthorization();
 app.MapControllers();
 
